e55e42a3b0dcf41276f51743f30b27700ff83d4b
Gustavo Marquezinho <gustavomarquezinho@gmail.com>
2023-12-28 15:03:50 -0300

    Add auto_clicker.py

---
+++ os/auto_clicker.py
@@ -0,0 +1,56 @@
+import time
+import threading
+from pynput.mouse import Button, Controller
+from pynput.keyboard import Listener, KeyCode
+ 
+ 
+delay = 1
+button = Button.left
+start_key = KeyCode(char='s')
+stop_key = KeyCode(char='s')
+exit_key = KeyCode(char='e')
+
+
+class ClickMouse(threading.Thread):
+    def __init__(self, delay, button):
+        super(ClickMouse, self).__init__()
+        self.delay = delay
+        self.button = button
+        self.running = False
+        self.program_run = True
+ 
+    def start_clicking(self):
+        self.running = True
+ 
+    def stop_clicking(self):
+        self.running = False
+ 
+    def exit(self):
+        self.stop_clicking()
+        self.program_run = False
+ 
+    def run(self):
+        while self.program_run:
+            while self.running:
+                mouse.click(self.button)
+                time.sleep(self.delay)
+            time.sleep(0.1)
+ 
+
+mouse = Controller()
+thread = ClickMouse(delay, button)
+thread.start()
+ 
+ 
+def on_press(key):
+    if key in (start_key, stop_key):
+        if thread.running:
+            thread.stop_clicking()
+        else:
+            thread.start_clicking()
+    elif key == exit_key:
+        thread.exit()
+        listener.stop()
+ 
+with Listener(on_press=on_press) as listener:
+    listener.join()
